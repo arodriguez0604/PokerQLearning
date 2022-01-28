c0b01e59676e164e1166b96fa100661f1b6d3179
Andrew <arodjr604@gmail.com>
2022-01-28 17:05:34 -0500

    Put ID in card class

--- poker.py
+++ poker.py
@@ -4,18 +4,20 @@ import math as m
 from multiprocessing.sharedctypes import Value
 from random import random
 
+#Suit (0-3); Value (1-13)
 class Card:
     suit = ''
     value = 0
+    id = 0
     
     def __init__(self, suit, value):
-        if (suit == 1):
+        if (suit == 0):
             self.suit = 'S'
-        elif (suit == 2):
+        elif (suit == 1):
             self.suit = 'D'
-        elif (suit == 3):
+        elif (suit == 2):
             self.suit = 'C'
-        elif (suit == 4):
+        elif (suit == 3):
             self.suit = 'H'
         else:
             print ("Error: Bad suit assignment [Poker.py]")
@@ -24,12 +26,8 @@ class Card:
             print ("Error: Bad value assignment [Poker.py]")
                         
         self.value = value
-    
-    def getSuit():
-        return self.suit
 
-    def getValue():
-        return value
+        self.id = value + (self - 1) * 13
 
 
 
@@ -39,7 +37,7 @@ deck = [None] * 52
 def createDeck():
     for x in 4:
         for y in 13:
-            deck[x][y] = Card(x, y)
+            deck[(13 * x) + ] = Card(x, y)
 
 def shuffle():
     for x in deck:
