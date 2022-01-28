c5d47cf6d857405ae3376a3ad3dfdd107974b494
robobruser <paulbruzzi@gmail.com>
2022-01-28 16:53:04 -0500

    make the deck work

--- poker.py
+++ poker.py
@@ -1,10 +1,18 @@
+import math as m
+from random import random
+
 class card:
     suit = ''
     value = 0
 
-# deck[4][13]
+deck = [None] * 52
 
 # def createDeck():
 #     for x in 4:
 #         for y in 13:
 #             deck[]
+
+def shuffle():
+    for x in deck:
+
+    random.int()
