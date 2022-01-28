f624f9ca6a2f856b0b2c59160886b0c892680876
robobruser <paulbruzzi@gmail.com>
2022-01-28 16:59:15 -0500

    shuffle method

--- poker.py
+++ poker.py
@@ -4,15 +4,13 @@ from random import random
 class card:
     suit = ''
     value = 0
+    id = 0
 
 deck = [None] * 52
 
-# def createDeck():
-#     for x in 4:
-#         for y in 13:
-#             deck[]
-
 def shuffle():
     for x in deck:
-
-    random.int()
+        card = random.int(1,52)
+        while card not in deck:
+            card = random.int(1,52)
+        deck[x] = card
